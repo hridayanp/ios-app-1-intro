134550e01d379c728f31e70412e07fce43c2156c
hridayanp <hridayanphukan123@gmail.com>
2024-11-21 23:35:32 +0530

    Initial Commit

---
+++ ios-magic-ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-magic-ball
+//
+//  Created by Hridayan Phukan on 21/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-magic-ballTests/ios_magic_ballTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_magic_ballTests.swift
+//  ios-magic-ballTests
+//
+//  Created by Hridayan Phukan on 21/11/24.
+//
+
+import Testing
+@testable import ios_magic_ball
+
+struct ios_magic_ballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
