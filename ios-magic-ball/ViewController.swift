0b4b8dc1195867daffecf0dc8d24cfd953e413c7
hridayanp <hridayanphukan123@gmail.com>
2024-11-21 23:52:15 +0530

    app completed

--- ios-magic-ball/ViewController.swift
+++ ios-magic-ball/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageBall: UIImageView!
+    
+    let ballArray = [
+            UIImage(named: "ball1"),
+            UIImage(named: "ball2"),
+            UIImage(named: "ball3"),
+            UIImage(named: "ball4"),
+            UIImage(named: "ball5")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageBall.image = UIImage(named: "ball1")
     }
 
-
+    @IBAction func askButtonHandler(_ sender: UIButton) {
+        // Generate a random index
+        let randomIndex = Int.random(in: 0...4)
+        
+        // Set the Image View to a random ball image
+        imageBall.image = ballArray[randomIndex]
+    }
+    
 }
 
